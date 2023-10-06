beb938ca2c01e21589281557d07d61610f854396
Shaikh Shahid Iqbal <35321907+nilgadia@users.noreply.github.com>
2023-10-06 10:07:08 +0300

    Delete debezium_embeded/target directory

--- debezium_embeded/target/src/main/java/com/example/target/module/student/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.target.module.student;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, Long> {
-}
